68a426212eadada363ed132678da58b24ebe3302
Teddy Walsh <teddyjwalsh@gmail.com>
2020-08-20 20:04:42 -0500

    Added bounty giving to HealthInterface

--- Source/AghsClone/HealthInterface.h
+++ Source/AghsClone/HealthInterface.h
@@ -36,10 +36,11 @@ class AGHSCLONE_API IHealthInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-    virtual void AddToHealth(float val)
+    virtual float AddToHealth(float val)
     {
         auto new_health = GetHealth() + val;
         SetHealth(new_health);
+        return new_health;
     }
 
     virtual float GetHealth() const = 0;
@@ -57,29 +58,43 @@ public:
     virtual float GetMaxHealth() const = 0;
 
     //virtual void ApplyDamage(float raw_value, DamageType damage_type)
-    virtual void ApplyDamage(DamageInstance& damage)
+    virtual void ApplyDamage(DamageInstance& damage, float& bounty)
     {
+        bounty = 0;
+        float new_health;
         switch (damage.damage_type)
         {
         case PureDamage:
         {
-            AddToHealth(-damage.value);
+            new_health = AddToHealth(-damage.value);
             break;
         }
         case PhysicalDamage:
         {
             float value = -damage.value*(1 - ((0.06 * GetArmor()) / (1 + 0.06 * abs(GetArmor()))));
-            AddToHealth(value);
+            new_health = AddToHealth(value);
             break;
         }
         case MagicDamage:
         {
             float value = -(1 - GetMagicResist()) * damage.value;
-            AddToHealth(value);
+            new_health = AddToHealth(value);
             break;
         }
         default:
             break;
+        }
+        if (new_health == 0.0)
+        {
+            AActor* bounty_actor = Cast<AActor>(this);
+            if (bounty_actor)
+            {
+                auto bounty_component = Cast<UBountyComponent>(bounty_actor->GetComponentByClass(UBountyComponent::StaticClass()));
+                if (bounty_comopnent)
+                {
+                    bounty = bounty_component->GetValue();
+                }
+            }
         }
     }
 };
